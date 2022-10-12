543bd14516f18170fcee49773fb9d2bce17e4e5e
Anna Zhurova <Zhurovaa@gmail.com>
2022-10-12 19:40:47 +0300

    ДЗ№6 Задание 4

--- oop_hw6.py
+++ oop_hw6.py
@@ -85,3 +85,84 @@ class Reviewer(Mentor):
     def __str__(self):
         res = f'Имя: {self.name}\nФамилия: {self.surname}'
         return res
+
+
+def grade_av_student(student_list, course):
+    sum = 0
+    count = 0
+    for person in student_list:
+        for i in person.grades[course]:
+            sum += i
+            count += 1
+    return round(sum / count, 1)
+
+def grade_av_lecturer(lecturer_list, course):
+    sum = 0
+    count = 0
+    for person in lecturer_list:
+        for i in person.grades1[course]:
+            sum += i
+            count += 1
+    return round(sum / count, 1)
+
+
+Kate_student = Student('Каte', 'Petrova', 'female')
+Roma_student = Student('Roma', 'Ivanov', 'Male')
+Kate_student.finished_courses += ['Java']
+Kate_student.courses_in_progress += ['Python', 'Git']
+Roma_student.finished_courses += ['Java', 'Git']
+Roma_student.courses_in_progress += ['Python']
+
+Anna_mentor = Mentor('Anna', 'Sirotina')
+Nata_mentor = Mentor('Nata', 'Dobry')
+
+Sasha_lecturer = Lecturer('Sasha', 'Belyi')
+Ulia_lecturer = Lecturer('Ulia', 'Vlasova')
+Sasha_lecturer.courses_attached += ['Python', 'Git']
+Ulia_lecturer.courses_attached += ['Python', 'Git']
+
+Irina_reviewer = Reviewer('Irina', 'Volkova')
+Olga_reviewer = Reviewer('Olga', 'Katina')
+Irina_reviewer.courses_attached += ['Python', 'Git']
+Olga_reviewer.courses_attached += ['Python', 'Git']
+
+Kate_student.rate_lecturer(Sasha_lecturer, 'Python', 10)
+Kate_student.rate_lecturer(Ulia_lecturer, 'Phython', 9)
+Roma_student.rate_lecturer(Ulia_lecturer, 'Python', 8)
+Kate_student.rate_lecturer(Ulia_lecturer, 'Python', 7)
+Roma_student.rate_lecturer(Ulia_lecturer, 'Python', 6)
+
+Irina_reviewer.rate_hw(Kate_student, 'Python', 6)
+Irina_reviewer.rate_hw(Roma_student, 'Python', 7)
+Irina_reviewer.rate_hw(Kate_student, 'Python', 8)
+Olga_reviewer.rate_hw(Kate_student, 'Python', 9)
+Olga_reviewer.rate_hw(Roma_student, 'Python', 9)
+Olga_reviewer.rate_hw(Kate_student, 'Git', 10)
+print('_' * 10)
+Kate_student.average_grade()
+Roma_student.average_grade()
+print('Студенты:')
+print(Kate_student)
+print(Roma_student)
+print('Лучшая успеваемость:')
+print(f'{Roma_student.surname if Kate_student < Roma_student else Kate_student.surname}')
+
+
+print('_' * 10)
+Sasha_lecturer.average_rate()
+Ulia_lecturer.average_rate()
+print(Sasha_lecturer < Ulia_lecturer)
+print(Sasha_lecturer)
+print(Ulia_lecturer)
+print(Irina_reviewer)
+print(Olga_reviewer)
+
+lecturer_list = [Sasha_lecturer, Ulia_lecturer]
+student_list = [Kate_student, Roma_student]
+course = 'Python'
+print('_' * 10)
+print('Средняя оценка студентов по курсу', course)
+print(grade_av_student(student_list, course))
+
+print('Средняя оценка лекторов по курсу', course)
+print(grade_av_student(student_list, course))
