5b06cf829383afb7e377a9d9ee5590d3421a29e1
Anna Zhurova <Zhurovaa@gmail.com>
2022-10-12 19:13:30 +0300

    ДЗ№6 Задание 3

--- oop_hw6.py
+++ oop_hw6.py
@@ -17,16 +17,49 @@ class Student:
         else:
             return 'Ошибка'
 
+    def average_grade(self):
+        self.average = round(sum(sum(self.grades.values(), [])) / len(sum(self.grades.values(), [])), 1)
+        return self.average
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Not a Student!')
+            return
+        return self.average_grade() < other.average_grade()
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nCредняя оценка: {self.average_grade()}\nКурсы в процессе: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}'
+        return res
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nАвтор курсов: {self.courses_attached} '
+        return res
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def average_rate(self):
+        self.average = round(sum(sum(self.grades.values(), [])) / len(sum(self.grades.values(), [])), 1)
+        return self.average
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Not a Lecturer!')
+            return
+        return self.average_rate() < other.average_rate()
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nCредняя оценка за лекции: {self.average_rate()} '
+        return res
 
 
 class Reviewer(Mentor):
@@ -40,3 +73,15 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return res
